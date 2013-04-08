58c6e685413bf7743bb9cfbeac364e560f9291d8
ちょまど <chomado@gmail.com>
2013-04-09 02:07:17 +0900

    The Fizzbuzz problem

---
+++ fizzBuzz.rb
@@ -0,0 +1,12 @@
+for i in 1.. 34 do
+  if i % 15 == 0 then
+    print "FizzBuzz "
+  elsif i % 5 == 0
+    print "Buzz "
+  elsif i % 3 == 0
+    print "Fizz "
+  else
+    printf("%d ", i)
+  end
+end
+print "\n"
